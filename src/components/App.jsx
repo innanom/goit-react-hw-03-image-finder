00a92544df25e25d02cae0c225ff76e1991af5dd
Inna Nomerovkina <inkosof@gmail.com>
2023-05-25 00:32:58 +0300

    component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import React, { Component } from 'react';
+import { Searchbar } from "./Searchbar/Searchbar";
+
+
+export class App extends Component {
+  state = {
+    images: null,
+  };
+
+  componentDidMount() {
+    fetch('https://pixabay.com/api/?key=35099720-4d8fe0d7f5adc4f66994afd08&q=yellow+flowers&image_type=photo')
+      .then(res => res.json()
+      .then(images => this.setState({images})));
+  }
+
+  render() {
+    return (
+      <div>
+        <Searchbar/>
+        {this.state.images && (<div>{this.state.images.totalHits}</div> )}
     </div>
-  );
+  );}
 };
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export class Searchbar extends Component {
+    state = {
+        images: ''
+    }
+
+    
+
+    render() {
+        return (
+            <header class="searchbar">
+            <form class="form">
+                <button type="submit" class="button">
+                <span class="button-label">Search</span>
+                </button>
+
+                <input
+                class="input"
+                type="text"
+                autocomplete="off"
+                autofocus
+                placeholder="Search images and photos"
+                />
+            </form>
+</header>
+        )
+    }
+}
